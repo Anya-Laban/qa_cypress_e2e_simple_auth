ff3b2444fd12b92693557d8dc623068381ebe5af
Anna Laban <laban.anya.a@gmail.com>
2025-07-31 19:03:09 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,39 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!' + '{enter}');
 
+    cy.url().should('include', '/secure');
+    cy.get('.flash.success')
+      .should('contain', 'You logged into a secure area!');
+  });
+
+  it('should assert validation errors with not valid creds', () => {
+    cy.get('#username').type('invalidUser');
+    cy.get('#password').type('' + '{enter}');
+
+    cy.url().should('include', '/login');
+    cy.get('.flash.error')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('should assert you successfully logged out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!' + '{enter}');
+
+    cy.url().should('include', '/secure');
+    cy.get('.flash.success')
+      .should('contain', 'You logged into a secure area!');
+
+    cy.get('.button').click();
+
+    cy.url().should('include', '/login');
+    cy.get('.flash.success')
+      .should('contain', 'You logged out of the secure area!');
   });
 });
